028eade9d3178ba49673504fcff0cb864b73104e
mambasa <101706131+kal990574@users.noreply.github.com>
2023-11-06 18:40:03 +0900

    init

---
+++ OpenCVTest/OpenCVTest/main.cpp
@@ -0,0 +1,44 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+using namespace cv;
+using namespace std;
+
+int main() {
+    Mat image = imread("/Users/mambasa/Desktop/picture/coins4.jpg");
+
+    Mat gray_image;
+    Mat blur_image;
+    Mat canny_image;
+    vector<Vec3f> result;
+    int cnt =0;
+    // 1.05좌 1우
+    namedWindow("Image");
+    imshow("Image", image);
+    
+    cvtColor(image, gray_image, COLOR_BGR2GRAY);
+    // Gaussian blur param 조작, 9x9 고정, sigma 값
+    GaussianBlur(gray_image, blur_image, Size(7, 7), 1, 0.387);
+    // thresholding param 조작 최소값, 최대값
+    Canny(blur_image, canny_image, 90, 150);
+    // Circle param 조작 dp, mindist, 높은 임계값, 크기가 작을수록 잘못된 원 더 감지, 최소원반경, 최대원반경
+    HoughCircles(canny_image, result, HOUGH_GRADIENT, 1, 84, 150, 40, 25, 80);
+
+    for (auto& c : result) {
+        circle(image, Point(c[0], c[1]), c[2], Scalar(255, 0, 255), 3, LINE_AA);
+        cnt++;
+    }
+    
+    cout << cnt;
+    namedWindow("Gray");
+    imshow("Gray", gray_image);
+    namedWindow("Blur");
+    imshow("Blur", blur_image);
+    namedWindow("Canny");
+    imshow("Canny", canny_image);
+    namedWindow("Circles");
+    imshow("Circles", image);
+
+    waitKey(0);
+}
---
+++ OpenCVTest/OpenCVTest/test.hpp
@@ -0,0 +1,13 @@
+//
+//  test.hpp
+//  OpenCVTest
+//
+//  Created by 이재현 on 2023/11/03.
+//
+
+#ifndef test_hpp
+#define test_hpp
+
+#include <stdio.h>
+
+#endif /* test_hpp */
